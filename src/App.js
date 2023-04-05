5d9e4bf9d0b0ee047ce6aa9d18647a4eb993b058
Konstantin <mke-62@ya.ru>
2023-04-05 15:40:33 +0300

    up
    p

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Layout from "./Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,15 @@
 import './App.css';
+import styled from 'styled-components';
+import All from "./pages/all"
+
 
 import {Outlet, NavLink} from "react-router-dom";
+
+const MenuStyleUl = styled.ul`
+      display: flex;
+      justify-content: space-evenly;
+      list-style: none;
+    `;
 
 
 const Layout = ({QQQ}) => {
@@ -38,12 +47,60 @@ const Layout = ({QQQ}) => {
             id: 6,
         }
     ];
+    const cars = [
+        {
+            id: 1,
+            label: 'AUDI',
+            model: 'tt',
+            power: 2,
+            year: 2005,
+            prise: 22000,
+            color: 'red',
+        },
+        {
+            id: 2,
+            label: 'BMW',
+            model: 'x6',
+            power: 3,
+            year: 2009,
+            prise: 32000,
+            color: 'white',
+        },
+        {
+            id: 3,
+            label: 'MAZDA',
+            model: '3',
+            power: 1.6,
+            year: 2010,
+            prise: 12000,
+            color: 'blue',
+        },
+        {
+            id: 4,
+            label: 'OPEL',
+            model: 'astra',
+            power: 1.8,
+            year: 2009,
+            prise: 32000,
+            color: 'green',
+        },
+        {
+            id: 5,
+            label: 'RENAUL',
+            model: 'logan',
+            power: 1.4,
+            year: 2020,
+            prise: 26000,
+            color: 'white',
+        },
+    ];
+
 
     return (
         <>
 
             <div>
-                <ul>
+                <MenuStyleUl>
                     {menus.map((menus) => (
                         <li key={menus.id}>
                             <NavLink as={NavLink} to={menus.links}>
@@ -51,12 +108,13 @@ const Layout = ({QQQ}) => {
                             </NavLink>
                         </li>
                     ))}
-                </ul>
+                </MenuStyleUl>
 
             </div>
 
 
             <Outlet/>
+            <All arr={cars}/>
 
 
         </>
